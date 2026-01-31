667c61108c70b2949d99ceab93e80254e7eda147
Abin Thomas <followgamer102@gmail.com>
2026-01-31 23:27:03 +0530

    conflicts

--- trpc/routers/_app.ts
+++ trpc/routers/_app.ts
@@ -6,9 +6,14 @@ import { chatRouter } from "./chat";
 import { contextRouter } from "./context";
 import { vulnerabilityRouter } from './vulnerability';
 import { alertRouter } from "./alert";
+<<<<<<< HEAD
 import { twilioRouter } from "./twilio.router";
 import { systemRouter } from "./system.router";
 import { hostRouter } from "./host";
+=======
+import { twilioRouter } from "./twilio.router";
+import { systemRouter } from "./system.router";
+>>>>>>> 32f9f88 (feat: voice call alert)
 
 export const appRouter = createTRPCRouter({
   test: publicProcedure.query(async (opts) => {
@@ -21,10 +26,12 @@ export const appRouter = createTRPCRouter({
   chat: chatRouter,
   context: contextRouter,
   alert: alertRouter,
-  twilio: twilioRouter,
-  system: systemRouter,
   host: hostRouter,
   vulnerability: vulnerabilityRouter
+=======
+  twilio: twilioRouter,
+  system: systemRouter,
+>>>>>>> 32f9f88 (feat: voice call alert)
 });
 
 export type AppRouter = typeof appRouter;
